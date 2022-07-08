f755c26e8b8b0777b0a3f5f21e5e42de81740b0a
ada <625507081@qq.com>
2022-07-08 10:03:17 +0800

    完成了个人中心功能开发

---
+++ assets/js/user/user_pwd.js
@@ -0,0 +1,38 @@
+$(function() {
+    var form = layui.form;
+    var layer = layui.layer;
+    //自定义验证规则
+    form.verify({
+            pwd: [
+                /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
+            ],
+            samePwd: function(value) {
+                if (value === $('[name=oldPwd]').val()) {
+                    return '新旧密码不能相同！'
+                }
+            },
+            rePwd: function(value) {
+                if (value !== $('[name=newPwd]').val()) {
+                    console.log($('[name=newPwd]').val());
+                    return '两次密码不一致！'
+                }
+            }
+        })
+        //监听表单的提交事件
+    $('.layui-form').on('submit', function(e) {
+        e.preventDefault();
+        $.ajax({
+            method: 'POST',
+            url: '/my/updatepwd',
+            data: $(this).serialize(),
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('修改密码失败！')
+                }
+                layer.msg('修改密码成功！')
+                $('.layui-form')[0].reset();
+            }
+        })
+
+    })
+})
